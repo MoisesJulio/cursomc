5b51a98cb5b83eff2e76ad5f7f68fea84cfe5f68
MoisesJulio <moisesmachava13@gmail.com>
2022-03-03 04:27:34 -0800

    criacao do projecto

---
+++ src/main/java/com/machava/machava/MachavaApplication.java
@@ -0,0 +1,13 @@
+package com.machava.machava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/machava/machava/MachavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machava.machava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
